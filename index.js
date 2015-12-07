68b0d212683c71dff098ba6a3c9221c489a43f82
nikolalsvk <nikolaseap@gmail.com>
2015-12-07 11:48:11 +0100

    Different file writing logic

--- index.js
+++ index.js
@@ -12,17 +12,21 @@ exports.handler = function(event, context) {
 
   var s3 = new AWS.S3({region: 'us-west-2'});
   var params = {Bucket: 'congrats-you-broke-the-build', Key: 'numbers.json'};
-  var file = fs.createWriteStream('/numbers.json');
 
   s3.getObject(params, { stream: true }, function(err, data) {
     if(err) console.log(err, err.stack); // an error has happened
-    data.createReadStream().pipe(file);
+
+    console.log("Successfully read file from S3");
+    fs.writeFile('numbers.json', data, function(err) {
+      if(err) console.log(err, err.stack);
+    });
+    console.log("Written contents of S3 file to numbers.json");
+
+    console.log("Finished GET numbers.json from S3 Bucket");
+
+    var numbersNonJson = fs.readFileSync("numbers.json");
+    var numbers = JSON.parse(numbersNonJson);
+
+    console.log("Numbers from S3: %j", numbers);
   });
-
-  console.log("Finished GET numbers.json from S3 Bucket");
-
-  var numbersNonJson = fs.readFileSync("numbers.json");
-  var numbers = JSON.parse(numbersNonJson);
-
-  console.log("Numbers from S3: %j", numbers);
 };
