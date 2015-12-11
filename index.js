5b146192453948f264e4e647d974458707083000
nikolalsvk <nikolaseap@gmail.com>
2015-12-11 09:57:29 +0100

    Refactor a bit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
 var AWS = require('aws-sdk');
 var twilio = require('twilio');
 
@@ -15,7 +14,7 @@ exports.handler = function(event, context) {
   var params = {Bucket: 'congrats-you-broke-the-build', Key: 'numbers.json'};
 
   s3.getObject(params, function(err, data) {
-    if(err) console.log(err, err.stack); // an error has happened
+    if(err) console.log(err, err.stack); // an error has happened on AWS
 
     console.log("Successfully read file from S3");
     var numbers = JSON.parse(data.Body);
@@ -23,33 +22,32 @@ exports.handler = function(event, context) {
 
     console.log("Finished GET numbers.json from S3 Bucket");
 
+    // twilio params
     var twilio_account_sid = numbers.twilio.twilio_account_sid;
     var twilio_auth_token = numbers.twilio.twilio_auth_token;
     var twilio_number = numbers.twilio.twilio_number;
-
-    console.log(twilio_account_sid);
 
     manipulateNumbers(numbers, twilio_account_sid, twilio_auth_token, twilio_number);
   });
 
   function manipulateNumbers(numbers, twilio_account_sid, twilio_auth_token, twilio_number) {
-    if(event.branch_name == "master" && event.result == "failed") {
+   if(event.branch_name == "master" && event.result == "failed") {
       var blame = event.commit.author_name;
       var blame_mail = event.commit.author_email;
 
+      // message that is sent to the one who broke the master branch
       var message = "Congrats " + blame + ", you managed to brake master branch on SemaphoreCI! Go and buy some kuglice for the office to make up."
 
       var client = twilio(twilio_account_sid, twilio_auth_token);
-      console.log(twilio_account_sid, twilio_auth_token, twilio_number);
 
       client.sendSms({
         to: numbers[blame_mail],
         from: twilio_number,
         body: message
-      }, function(err, responseData) { //this function is executed when a response is received from Twilio
+      }, function(err, responseData) { // this function is executed when a response is received from Twilio
         if (!err) {
           console.log(responseData);
-          context.done(null, "Message sent!");
+          context.done(null, "Message sent to " + numbers[blame_mail] + "!");
         } else {
           console.log(err);
           context.done(null, "Error has happened, message was not sent!");
