4ad5e1dd5a65bbcb944fb403c76c32c766dcab04
nikolalsvk <nikolaseap@gmail.com>
2015-12-11 10:15:09 +0100

    Refactor a bit more

--- index.js
+++ index.js
@@ -22,15 +22,10 @@ exports.handler = function(event, context) {
 
     console.log("Finished GET numbers.json from S3 Bucket");
 
-    // twilio params
-    var twilio_account_sid = numbers.twilio.twilio_account_sid;
-    var twilio_auth_token = numbers.twilio.twilio_auth_token;
-    var twilio_number = numbers.twilio.twilio_number;
-
-    manipulateNumbers(numbers, twilio_account_sid, twilio_auth_token, twilio_number);
+    manipulateNumbers(numbers);
   });
 
-  function manipulateNumbers(numbers, twilio_account_sid, twilio_auth_token, twilio_number) {
+  function manipulateNumbers(numbers) {
    if(event.branch_name == "master" && event.result == "failed") {
       var blame = event.commit.author_name;
       var blame_mail = event.commit.author_email;
@@ -38,21 +33,31 @@ exports.handler = function(event, context) {
       // message that is sent to the one who broke the master branch
       var message = "Congrats " + blame + ", you managed to brake master branch on SemaphoreCI! Go and buy some kuglice for the office to make up."
 
-      var client = twilio(twilio_account_sid, twilio_auth_token);
-
-      client.sendSms({
-        to: numbers[blame_mail],
-        from: twilio_number,
-        body: message
-      }, function(err, responseData) { // this function is executed when a response is received from Twilio
-        if (!err) {
-          console.log(responseData);
-          context.done(null, "Message sent to " + numbers[blame_mail] + "!");
-        } else {
-          console.log(err);
-          context.done(null, "Error has happened, message was not sent!");
-        }
-      });
+      twilioHandler(numbers, message);
     };
+  };
+
+  function twilioHandler(numbers, message) {
+    // twilio params
+    var twilio_account_sid = numbers.twilio.twilio_account_sid;
+    var twilio_auth_token = numbers.twilio.twilio_auth_token;
+    var twilio_number = numbers.twilio.twilio_number;
+
+    var client = twilio(twilio_account_sid, twilio_auth_token);
+
+    // Send SMS
+    client.sendSms({
+      to: numbers[blame_mail],
+      from: twilio_number,
+      body: message
+    }, function(err, responseData) { // this function is executed when a response is received from Twilio
+      if (!err) {
+        console.log(responseData);
+        context.done(null, "Message sent to " + numbers[blame_mail] + "!");
+      } else {
+        console.log(err);
+        context.done(null, "Error has happened, message was not sent!");
+      }
+    });
   };
 }
