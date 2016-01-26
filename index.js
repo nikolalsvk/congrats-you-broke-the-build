3bc2dbb984b93c1c67830150b9c9362298fa3462
nikolalsvk <nikolaseap@gmail.com>
2016-01-26 12:21:19 +0100

    Check if build on master has failed, then load file from S3

--- index.js
+++ index.js
@@ -8,35 +8,29 @@ exports.handler = function(event, context) {
     s3: '2006-03-01'
   }
 
-  console.log("GET numbers.json from S3 bucket");
+  if(event.branch_name == "master" && event.result == "failed") {
+    console.log("GET numbers.json from S3 bucket");
 
-  var s3 = new AWS.S3({region: 'us-west-2'});
-  var params = {Bucket: 'congrats-you-broke-the-build', Key: 'numbers.json'};
+    var s3 = new AWS.S3({region: 'us-west-2'});
+    var params = {Bucket: 'congrats-you-broke-the-build', Key: 'numbers.json'};
 
-  s3.getObject(params, function(err, data) {
-    if(err) console.log(err, err.stack); // an error has happened on AWS
+    s3.getObject(params, function(err, data) {
+      if(err) console.log(err, err.stack); // an error has happened on AWS
 
-    console.log("Successfully read file from S3");
-    var numbers = JSON.parse(data.Body);
-    console.log(numbers);
+      var numbers = JSON.parse(data.Body);
+      console.log(numbers);
 
-    console.log("Finished GET numbers.json from S3 Bucket");
+      console.log("Finished GET numbers.json from S3 Bucket");
 
-    manipulateNumbers(numbers);
-  });
-
-  function manipulateNumbers(numbers) {
-   if(event.branch_name == "master" && event.result == "failed") {
       var blame = event.commit.author_name;
-
       // message that is sent to the one who broke the master branch
-      var message = "Congrats " + blame + ", you managed to brake master branch on SemaphoreCI! Go and buy some kuglice for the office to make up."
+      var message = "Congrats " + blame + ", you managed to brake master branch on SemaphoreCI! Go and buy some kuglice for the office to make up.";
 
       twilioHandler(numbers, message);
-    } else {
-      context.done(null, "Build passed!");
-    }
-  };
+    });
+  } else {
+    context.done(null, "Build passed!");
+  }
 
   function twilioHandler(numbers, message) {
     var blame_mail = event.commit.author_email;
